fd5d5b2c395b77795c84fb0b439fa651e7e7625a
Ralph Hendriks <mail@ralphhendriks.nl>
2023-12-10 08:41:07 +0100

    Day 9

---
+++ day09/Program.fs
@@ -0,0 +1,25 @@
+﻿open System.IO
+
+let inputLines = File.ReadAllLines("input.txt") |> Array.toList
+
+let parseLine (s: string) = s.Split(" ") |> Array.toList |> List.map int
+
+let rec extrapolate (history: int list) =
+    match (List.forall ((=) 0) history) with
+    | true -> 0
+    | false ->
+        let delta = history |> List.pairwise |> List.map (fun (x, y) -> y - x) |> extrapolate
+        delta + List.last history
+
+let part1 = inputLines |> List.map (parseLine >> extrapolate) |> List.sum
+printfn "Answer part 1: %i" part1
+
+let rec extrapolateBack (history: int list) =
+    match (List.forall ((=) 0) history) with
+    | true -> 0
+    | false ->
+        let delta = history |> List.pairwise |> List.map (fun (x, y) -> y - x) |> extrapolateBack
+        (List.head history) - delta
+
+let part2 = inputLines |> List.map (parseLine >> extrapolateBack) |> List.sum
+printfn "Answer part 2: %i" part2
